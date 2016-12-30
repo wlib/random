c1f97a54601af1b0fe727ebfc67ee7c572be7247
Daniel Ethridge <danielethridge@icloud.com>
2016-12-29 20:19:30 -0500

    refreshed install script

--- install.sh
+++ install.sh
@@ -1,35 +1,51 @@
 #!/bin/sh
-# packages to install on every new debian/ubuntu system I get
+# Packages to install on every new debian/ubuntu system
 
-# on systems without apt
+# First install apt and then update/upgrade everything
 sudo apt-get install apt --assume-yes
 sudo apt update --assume-yes
 sudo apt upgrade --assume-yes
-wget -O /tmp/chrome.deb https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-sudo dpkg -i /tmp/chrome.deb
-sudo apt-get install -f --assume-yes
-sudo apt install preload --assume-yes
-sudo apt install guake --assume-yes
-sudo apt install blender --assume-yes
-sudo apt install inkscape --assume-yes
-sudo apt install audacity --assume-yes
-sudo apt install gimp --assume-yes
-sudo apt install nautilus-dropbox --assume-yes
-sudo apt install gparted --assume-yes
-sudo apt install cmake --assume-yes
-sudo apt install git --assume-yes
-sudo apt install ruby --assume-yes
+
+# Functions to make auto-installation simple
+inst() {
+  sudo apt install $1 --assume-yes
+}
+
+debinst() {
+  wget $1 -O /tmp/install.deb
+  sudo dpkg -i /tmp/install.deb
+  inst -f
+}
+
+# Install a list of programs from repo
+inst preload
+inst guake
+inst blender
+inst inkscape
+inst audacity
+inst gimp
+inst nautilus-dropbox
+inst gparted
+inst cmake
+inst git
+inst ruby
+inst node
+
+# Download and install these
+debinst https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb 
+debinst https://vscode-update.azurewebsites.net/latest/linux-deb-x64/stable
+
+# Add and install
 sudo add-apt-repository 'deb http://www.mediahuman.com/packages/ubuntu trusty main'
 sudo apt-key adv --keyserver pgp.mit.edu --recv-keys 7D19F1F3
 sudo apt-get update
-sudo apt install youtube-to-mp3 --assume-yes
+inst youtube-to-mp3
 
-# run programs to easily add them all to dock
-
-google-chrome #web browser
-blender #3d software
-inkscape #vector image editor
-audacity #audio editor
-gimp #image editor
-guake #amazingly useful drop-down terminal
-#nautilus-dropbox for dropbox file sync, set up window automatically opens on install
+# Run the new programs to easily add them all to dock
+google-chrome
+blender
+inkscape
+audacity
+gimp
+guake
+code
